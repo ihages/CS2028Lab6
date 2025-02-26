6b7ebce3b274cc13edfb8c6ca41b1a0c81d6d643
ihages <hagemaij@mail.uc.edu>
2025-02-26 14:55:17 -0500

    copied code to stack.h

--- stack.h
+++ stack.h
@@ -1,1 +1,35 @@
 #pragma once
+
+template <T>
+class Stack {
+private:
+	T* data[size];
+	int index;
+
+
+public:
+
+	? ? Stack(int inSize) : size(inSize) {}
+
+	void push(T inVal) {
+
+	}
+
+	T pop() {
+
+	}
+
+	T top() {
+
+	}
+
+	int length() {
+
+	}
+
+	void emptyStack() {
+
+	}
+
+
+};
