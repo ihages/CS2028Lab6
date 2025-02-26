d736737be31b08ea9a145fc6331b90c8c8fe2c21
ihages <hagemaij@mail.uc.edu>
2025-02-26 15:21:32 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,1 +1,16 @@
 #pragma once
+
+template<class T>
+class Queue {
+private:
+	T words[size];
+	int index = 0;
+public:
+	void enqueue(T inVal);
+	T dequeue();
+	T peek();
+	bool isEmpty();
+	bool isFUll();
+	Void makeEmpty();
+
+};
